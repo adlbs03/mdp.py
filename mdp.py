851487b68de0cb167d5201ea088f637e9c175acc
adlbs03 <adrienlebois@hotmail.com>
2026-03-24 11:51:30 +0100

    prise en charge des caractères spéciaux, et vérification majuscule pour plus de sécurité

--- mdp.py
+++ mdp.py
@@ -3,10 +3,15 @@ import string
 alpha_lower = list(string.ascii_lowercase)
 alpha_upper = list(string.ascii_uppercase)
 
+special_chars = set('[@_!#$%^&*()<>?/\\|}{~:]')
+
 password = input("Enter a password : ")
 
 while len(password) < 6:
-    password = input("Enter a password : ")
+    password = input("Enter a password with more than 6 chars : ")
+
+while not any(c.isupper() for c in password):
+     password = input("Enter a password with a minimum of one maj : ")
 
 decale_lower = alpha_lower[3:] + alpha_lower[:3]
 decale_upper = alpha_upper[3:] + alpha_upper[:3]
@@ -16,6 +21,7 @@ like_upper = dict(zip(alpha_upper, decale_upper))
 
 result_alpha = ""
 result_digit = ""
+result_spe = ""
 
 for c in password:
     if c.isalpha() and c.islower():
@@ -24,8 +30,10 @@ for c in password:
         result_alpha += like_upper[c]
     elif c.isdigit():
         result_digit += c * 2
+    elif c in special_chars:
+        result_spe += c
     else:
         result_alpha += c
 
-print(result_alpha + result_digit)
+print(result_alpha + result_digit + result_spe)
 
